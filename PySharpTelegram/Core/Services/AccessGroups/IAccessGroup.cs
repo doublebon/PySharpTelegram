b9fc32dc8bd5e0b1795e59ac6dacdd5841efe27f
doublebon <dbondbon@yandex.ru>
2023-11-22 18:09:21 +0300

    add access groups

---
+++ PySharpTelegram/Core/Services/AccessGroups/IAccessGroup.cs
@@ -0,0 +1,10 @@
+using Telegram.Bot.Types;
+
+namespace PySharpTelegram.Core.Services.AccessGroups;
+
+public interface IAccessGroup
+{
+    public Task<User[]> GetGroupMembersAsync(params string[] accessGroupName);
+    public Task<bool> AddMembersToGroupAsync<T>(string accessGroupName, params User[] members);
+    public Task<bool> RemoveMemberFromGroupAsync<T>(string accessGroupName, params User[] members);
+}
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -4,7 +4,9 @@ using PySharpTelegram.Core.Config;
 using PySharpTelegram.Core.Extensions;
 using PySharpTelegram.Core.Handlers;
 using PySharpTelegram.Core.Services.Abstract;
+using PySharpTelegram.Core.Services.AccessGroups;
 using Telegram.Bot;
+using TestApp.AccessGroups;
 using TestApp.Connector;
 
 var host = Host.CreateDefaultBuilder(args)
@@ -24,6 +26,7 @@ var host = Host.CreateDefaultBuilder(args)
         services.AddScoped<ReceiverService>();
         services.AddHostedService<PollingService>();
         services.AddSingleton<MessageAttributesHandler>();
+        services.AddSingleton<IAccessGroup, AccessGroup>();
         services.AddSingleton<InlineAttributesHandler>();
         services.AddSingleton<AbstractExternalConnector, ExternalConnector>(_ => new ExternalConnector("Chat"));
     })
