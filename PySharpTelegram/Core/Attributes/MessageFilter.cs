82af0843fe9d6283a51dfd39b89e5e83c5201612
doublebon <dbondbon@yandex.ru>
2024-05-11 20:05:07 +0300

    add additional constructors for filer attributes

--- PySharpTelegram/Core/Attributes/MessageFilter.cs
+++ PySharpTelegram/Core/Attributes/MessageFilter.cs
@@ -5,6 +5,13 @@ namespace PySharpTelegram.Core.Attributes;
 
 public abstract class MessageFilter
 {
+    public interface ITextType
+    {
+        CompareType CompareType { get; }
+        SearchType SearchType { get; }
+        IEnumerable<string> Text { get; }
+    }
+    
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
     public class ContentTypeAttribute(params MessageType[] type) : Attribute
     {
@@ -14,13 +21,6 @@ public abstract class MessageFilter
     [AttributeUsage(AttributeTargets.Method)]
     public class AnyAttribute : Attribute;
     
-    public interface ITextType
-    {
-        CompareType CompareType { get; }
-        SearchType SearchType { get; }
-        IEnumerable<string> Text { get; }
-    }
-    
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
     public class TextAttribute(CompareType compareType, SearchType searchType, params string[] text)
         : Attribute, ITextType
@@ -28,6 +28,8 @@ public abstract class MessageFilter
         public CompareType CompareType { get; } = compareType;
         public SearchType SearchType { get; } = searchType;
         public IEnumerable<string> Text { get; } = text;
+        
+        public TextAttribute(params string[] text) : this(CompareType.Equals, SearchType.AnyOf, text){}
     }
     
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
@@ -37,5 +39,7 @@ public abstract class MessageFilter
         public CompareType CompareType { get; } = compareType;
         public SearchType SearchType { get; } = searchType;
         public IEnumerable<string> Text { get; } = text;
+        
+        public ReplyOnTextAttribute(params string[] text) : this(CompareType.Equals, SearchType.AnyOf, text){}
     }
 }
